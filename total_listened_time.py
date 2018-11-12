2daeebe875c0aa60b5abf601a06c6fbdf4aa2ecc
Gladys Baudet <gladys.baudet@gmail.com>
2018-11-12 16:01:02 -0500

    modif overlap -- working?

--- total_listened_time.py
+++ total_listened_time.py
@@ -2,12 +2,15 @@ import os
 import sys
 import re
 import pandas as pd
+import csv
 
 
 interval_regx = re.compile("\\x15(\d+_\d+)\\x15")
 
 
-def pull_regions(path):
+def pull_regions(path, month):
+    if month < 8:
+        raise Exception("No subregions in this file")
     begin_extras = []
     end_extras = []
     begin_makeup = []
@@ -21,6 +24,7 @@ def pull_regions(path):
 
     with open(path, "rU") as input:
         lines = []
+        print("here")
         for line in input:
             if "begin extra time" in line:
                 ts = get_last_timestamp(lines)
@@ -40,34 +44,60 @@ def pull_regions(path):
             elif "end skip" in line:
                 ts = get_last_timestamp(lines)
                 end_skip.append(ts)
+
             elif "subregion" in line and "starts" in line:
-                ts = get_last_timestamp(lines)
-                begin_subreg.append(ts)
+                if ("ranked 1" in line or "ranked 2" in line or "ranked 3" in line):
+                    ts = get_line_timestamp(line)
+                    begin_subreg.append(ts)
+                elif ("ranked 4" in line and month<14):
+                    ts = get_line_timestamp(line)
+                    begin_subreg.append(ts)
+
             elif "subregion" in line and "ends" in line:
-                ts = get_last_timestamp(lines)
-                end_subreg.append(ts)
+                if ("ranked 1" in line or "ranked 2" in line or "ranked 3" in line):
+                    ts = get_line_timestamp(line, start = False)
+                    end_subreg.append(ts)
+                elif ("ranked 4" in line and month<14):
+                    ts = get_line_timestamp(line, start = False)
+                    end_subreg.append(ts)
+
             elif "silence" in line and "starts" in line:
-                ts = get_last_timestamp(lines)
+                ts = get_line_timestamp(line)
                 begin_sil.append(ts)
             elif "silence" in line and "ends" in line:
-                ts = get_last_timestamp(lines)
+                ts = get_line_timestamp(line, start = False)
                 end_sil.append(ts)
             else:
                 lines.append(line)
 
+    end_sil = list(set(end_sil))
     if len(begin_extras) != len(end_extras):
+        print(begin_extras, end_extras)
         raise Exception("EXTRA: begin count does not match end count")
     elif len(begin_makeup) != len(end_makeup):
+        print(begin_makeup, end_makeup)
         raise Exception("MAKEUP: begin count does not match end count")
     elif len(begin_skip) != len(end_skip):
         raise Exception("SKIP: begin count does not match end count")
     elif len(begin_sil) != len(end_sil):
+        print(begin_sil, end_sil)
         raise Exception("SILENCE: begin count does not match end count")
     else:
         return begin_extras, end_extras, begin_makeup, \
                end_makeup, begin_skip, end_skip,\
                begin_subreg, end_subreg, begin_sil,\
                end_sil
+
+
+def get_line_timestamp(line, start = True):
+    words = line.split()
+    print("issue here")
+    if start:
+        print("start", len(words), words.index("starts"))
+        return int(float(words[words.index("starts")+2]))
+    else:
+        print("end",len(words), words.index("ends"))
+        return int(float(words[words.index("ends")+2]))
 
 
 def get_last_timestamp(lines):
@@ -89,7 +119,8 @@ def sum_regions(begins, ends):
 def sil_subr_overlap_sum(month,
                          b_sil, e_sil,
                          b_sub, e_sub,
-                         b_mak, e_mak):
+                         b_mak, e_mak,
+                         b_ext, e_ext):
 
     """
     sum of all silence +subregion overlap time
@@ -106,75 +137,102 @@ def sil_subr_overlap_sum(month,
     sils = zip(b_sil, e_sil)
     makeups = zip(b_mak, e_mak)
 
+    overlap = 0
     for silreg in sils:
-        region_overlap(silreg, subregs)
+        overlap += region_overlap(silreg, subregs)
+        overlap += region_overlap(silreg, makeups)
+        overlap += region_overlap(silreg, extra)
+
+    return overlap
 
 def region_overlap(sil, subregs):
+    sum = 0
     for reg in subregs:
-        if reg[0] < sil[0] < reg[1]:
-            if reg[0] < sil[1] < reg[1]:
-                print
+        if reg[0] < sil[0] < reg[1] < sil[1]:
+            return(reg[1] - sil[0])
+        elif reg[0] < sil[0] < sil[1] < reg[1]:
+            return(sil[1] - sil[0])
+        elif sil[0] < reg[0] < sil[1] < reg[1]:
+            return(sil[1] - reg[0])
+
 
 
 
 if __name__ == "__main__":
 
-    input = sys.argv[1]
+    file = sys.argv[1]
     # subregs = sys.argv[2]
-
-    files = [os.path.join(input, x) for x in os.listdir(input)]
 
     results = []
 
+    fname = os.path.basename(file)
+    month = int(fname[3:5])
 
-    for file in files:
-        fname = os.path.basename(file)
+    extra_sum = 0
+    makeup_sum = 0
+    skip_sum = 0
+    overlap_sum = 0
+    subr_sum = 0
+    try:
+        print("HERE")
+        b_extra, e_extra, b_makup, e_makup, \
+        b_skip, e_skip, b_subr, e_subr, b_sil, \
+        e_sil  = pull_regions(file, month)
+        if b_extra:
+            try:
+                extra_sum = sum_regions(b_extra, e_extra)
+                print("1")
+            except Exception as e:
+                print fname
+                print e
+                print
+        if b_makup:
+            try:
+                makeup_sum = sum_regions(b_makup, e_makup)
+                print("2")
+            except Exception as e:
+                print fname
+                print e
+                print
+        if b_skip:
+            try:
+                skip_sum = sum_regions(b_skip, e_skip)
+                print("3")
+            except Exception as e:
+                print fname
+                print e
+                print
+        if b_sil:
+            try:
+                overlap_sum = sil_subr_overlap_sum(int(fname[3:5]),
+                                                   b_sil, e_sil,
+                                                   b_subr, e_subr,
+                                                   b_makup, e_makup)
 
-        extra_sum = 0
-        makeup_sum = 0
-        skip_sum = 0
-        try:
-            b_extra, e_extra, b_makup, e_makup, \
-            b_skip, e_skip, b_subr, e_subr, b_sil, \
-            e_sil  = pull_regions(file)
-            if b_extra:
-                try:
-                    extra_sum = sum_regions(b_extra, e_extra)
-                except Exception as e:
-                    print fname
-                    print e
-                    print
-            if b_makup:
-                try:
-                    makeup_sum = sum_regions(b_makup, e_makup)
-                except Exception as e:
-                    print fname
-                    print e
-                    print
-            if b_skip:
-                try:
-                    skip_sum = sum_regions(b_skip, e_skip)
-                except Exception as e:
-                    print fname
-                    print e
-                    print
-            if b_sil:
-                try:
-                    overlap_sum = sil_subr_overlap_sum(int(fname[3:5]),
-                                                       b_sil, e_sil,
-                                                       b_subr, e_subr,
-                                                       b_makup, e_makup)
-                except:
-                    print
+                print("4")
+            except:
+                print
+        if b_subr:
+            try:
+                subr_sum = sum_regions(b_subr, e_subr)
+                print("5")
+            except Exception as e:
+                print fname
+                print e
+                print
 
-            results.append((os.path.basename(file)[:5], extra_sum, makeup_sum, skip_sum))
+        results.append((os.path.basename(file)[:5], extra_sum, makeup_sum, skip_sum, overlap_sum, subr_sum))
 
-        except Exception as e:
-            print fname
-            print e
-            print
+    except Exception as e:
+        print fname
+        print e
+        print
 
+    with open(r"total_listened_time.csv", 'a') as f:
+        writer = csv.writer(f)
+        for row in results:
+            writer.writerow(row)
 
-
-    df = pd.DataFrame(results, columns=["file", "extra", "makeup", "skip"])
-    df.to_csv("total_listened_time.csv", index=False)
+    # df = pd.DataFrame(results, columns=["file", "extra", "makeup", "skip", "overlap", "subregions"])
+    # print(df)
+    # df.to_csv("total_listened_time.csv", index=False)
