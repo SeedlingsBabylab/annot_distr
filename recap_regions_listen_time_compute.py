1be31c8a5c6e8e64cdfa49aaddcfb64531ecbdb5
Sarp Uner <sarpim@gmail.com>
2020-09-01 15:46:32 -0400

    Added the ability to process a single file for testing

--- recap_regions_listen_time_compute.py
+++ recap_regions_listen_time_compute.py
@@ -10,6 +10,13 @@ from settings import *
 from funcs import *
 from listen_time import total_listen_time
 from check_errors import sequence_missing_repetition_entry_alert
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Compute listened time for the corpus.')
+    parser.add_argument('input_file', help='Either a path file containing a path for each cha file, one path per line, OR, a single cha file.')
+    parser.add_argument('--output_path', help='Optional output directory to output the reports/csvs/etc.', default='output')
+    return parser.parse_args()
 
 
 
@@ -75,51 +82,60 @@ def process_single_file(file, file_path=cha_structure_path):
 
 
 if __name__ == "__main__":
-    path_file = sys.argv[1]
-    files = []
-    with open(path_file) as f:
-        for path in f.readlines():
-            path = path.strip()
-            files.append(path)
-    print("Expected to process {} cha files".format(len(files)))
-    # Create output folder if it does not exist
-    try:
-        output_path = sys.argv[2]
-        if output_path.startswith('--'):
-            output_path = 'output'
-    except:
-        output_path = 'output'
-    if not os.path.isdir(output_path):
-        os.mkdir(output_path)
-    if not os.path.isdir(os.path.join(output_path, 'cha_structures')):
-        os.mkdir(os.path.join(output_path, 'cha_structures'))
-    cha_structure_path = os.path.join(output_path, 'cha_structures')
+    args = get_args()
 
-    if '--fast' in sys.argv:
-        global manager
-        manager = Manager()
-        file_with_error = manager.list()
-        listen_time_summary = manager.list()
-        original_sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
-        p = Pool(6)
-	signal.signal(signal.SIGINT, original_sigint_handler)
-        try:
-            res = p.map(process_single_file, files)
-        except KeyboardInterrupt:
-            print("Caught KeyboardInterrupt, terminating workers")
-            p.terminate()
-        else:
-            print("Normal termination")
-            p.close()
-        p.join()
+    if os.path.splitext(args.input_file)[-1] == '.cha':
+        listen_time_summary = []
+        file_with_error = []
+        process_single_file(args.input_file)
 
     else:
-        file_with_error = []
-        listen_time_summary = []
 
-        for file in files:
-            process_single_file(file, cha_structure_path)
-        
-    # We output the findings.
-    output(file_with_error, listen_time_summary, output_path)
+        path_file = args.input_file
+        files = []
+        with open(path_file) as f:
+            for path in f.readlines():
+                path = path.strip()
+                files.append(path)
+        print("Expected to process {} cha files".format(len(files)))
+        # Create output folder if it does not exist
+        try:
+            output_path = sys.argv[2]
+            if output_path.startswith('--'):
+                output_path = 'output'
+        except:
+            output_path = 'output'
+        if not os.path.isdir(output_path):
+            os.mkdir(output_path)
+        if not os.path.isdir(os.path.join(output_path, 'cha_structures')):
+            os.mkdir(os.path.join(output_path, 'cha_structures'))
+        cha_structure_path = os.path.join(output_path, 'cha_structures')
+
+        if '--fast' in sys.argv:
+            global manager
+            manager = Manager()
+            file_with_error = manager.list()
+            listen_time_summary = manager.list()
+            original_sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
+            p = Pool(6)
+            signal.signal(signal.SIGINT, original_sigint_handler)
+            try:
+                res = p.map(process_single_file, files)
+            except KeyboardInterrupt:
+                print("Caught KeyboardInterrupt, terminating workers")
+                p.terminate()
+            else:
+                print("Normal termination")
+                p.close()
+            p.join()
+
+        else:
+            file_with_error = []
+            listen_time_summary = []
+
+            for file in files:
+                process_single_file(file, cha_structure_path)
+            
+        # We output the findings.
+    output(file_with_error, listen_time_summary, args.output_path)
 
